4694f13c0a498534248a0d23a7ffd15e7290b300
Eren Can <78620143+gyspy5@users.noreply.github.com>
2023-12-17 20:17:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from PIL import Image
 
-image_path_list = ['enter.png']
+image_path_list = ['enter.png'] #Enter your png names in here (which ones you want to make gif).
 
 image_list = [Image.open(file) for file in image_path_list]
 
 image_list[0].save(
-    'test.gif',
+    'test.gif', #Your gif name.
     save_all=True,
     append_images=image_list[1:],
     duration=500,
